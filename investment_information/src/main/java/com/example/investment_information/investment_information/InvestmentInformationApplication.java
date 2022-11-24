e772d30d4f73c168d6f015cd0ae37759aeeb6c7e
GwanHwi1 <ghc98@naver.com>
2022-11-24 17:00:57 +0900

    investment_information

---
+++ investment_information/src/main/java/com/example/investment_information/investment_information/InvestmentInformationApplication.java
@@ -0,0 +1,13 @@
+package com.example.investment_information.investment_information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentInformationApplication.class, args);
+	}
+
+}
---
+++ investment_information/src/test/java/com/example/investment_information/investment_information/InvestmentInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.investment_information.investment_information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
